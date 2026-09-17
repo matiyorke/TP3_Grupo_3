e4e3a15bef00db71cdb9830e5bcd07d146e06e79
eliancarrasco552-netizen <eliancarrasco552@gmail.com>
2026-09-16 22:08:20 -0300

    hice la entidad categoria

--- src/entidad/Categoria.java
+++ src/entidad/Categoria.java
@@ -2,7 +2,39 @@ package entidad;
 
 public class Categoria {
 
-	public Categoria() {}
+	private int idCategoria;
+	private String nombre;
+	
+	public Categoria()
+	{
+		
+	}
+	
+	public Categoria(int idCategoria, String nombre) {
+		this.idCategoria = idCategoria;
+		this.nombre = nombre;
+	}
+
+	public int getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Categoria [idCategoria=" + idCategoria + ", nombre=" + nombre + "]";
+	}
 
 	
 }
